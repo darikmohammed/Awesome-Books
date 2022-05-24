2248abcc7dc97aba97da52ee8fdc4e0274c8c198
darikmohammed <r.m.darik@gmail.com>
2022-05-24 22:55:53 +0300

    Refactor Js with class

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
-let booksList = [
-  { autor: 'adfjlaf', title: 'afdfaf' },
-  { autor: 'adfjlaf', title: 'other' },
-];
-
-const setBooksList = (books) => {
-  localStorage.setItem('Books', JSON.stringify(books));
-};
-const getBooksList = () => {
-  const books = JSON.parse(localStorage.getItem('Books'));
-  if (books) {
-    return books;
+class AwsomeBooks {
+  constructor() {
+    this.booksList = [];
   }
-  return [];
-};
 
-const newCollection = (bookValues) => {
-  booksList = getBooksList();
-  booksList.push(bookValues);
-  setBooksList(booksList);
-};
+  #SetBooksList = (books) => {
+    localStorage.setItem('Books', JSON.stringify(books));
+  };
 
-const showBooks = () => {
-  const books = getBooksList();
-  const htmlbooks = document.querySelector('.books');
-  htmlbooks.innerHTML = '';
-  for (let i = 0; i < books.length; i += 1) {
-    htmlbooks.innerHTML += `
-        <div class="book">
-        <h3>${books[i].title}</h3>
-        <p>${books[i].autor}</p>
-        <button type="button" onClick= "deleteCollection(${i})">Remove</button>
-        `;
-  }
-};
+  #getBooksList = () => {
+    const books = JSON.parse(localStorage.getItem('Books'));
+    if (books) {
+      return books;
+    }
+    return [];
+  };
 
-const deleteCollection = (bookIndex) => {
-  if (bookIndex !== null) {
-    const books = getBooksList();
+  showBooks = () => {
+    const books = this.#getBooksList();
+    const htmlbooks = document.querySelector('.books');
+    htmlbooks.innerHTML = '';
+    for (let i = 0; i < books.length; i += 1) {
+      htmlbooks.innerHTML += `
+          <div class="book">
+          <h3>${books[i].title}</h3>
+          <p>${books[i].autor}</p>
+          <button type="button" onClick= "remove(${i})">Remove</button>
+          `;
+    }
+  };
 
-    const bookRemoved = books.filter((item, key) => {
-      if (key !== bookIndex) {
-        return true;
-      }
-      return null;
-    });
-    setBooksList(bookRemoved);
-    showBooks();
-  }
-};
-deleteCollection(-1);
+  newCollection = (bookValues) => {
+    this.booksList = this.#getBooksList();
+    this.booksList.push(bookValues);
+    this.#SetBooksList(this.booksList);
+  };
+
+  deleteCollection = (bookIndex) => {
+    if (bookIndex !== null) {
+      const books = this.#getBooksList();
+
+      const bookRemoved = books.filter((item, key) => {
+        if (key !== bookIndex) {
+          return true;
+        }
+        return null;
+      });
+      this.#SetBooksList(bookRemoved);
+      this.showBooks();
+    }
+  };
+}
+
+const awsomeBooks = new AwsomeBooks();
+
 const form = document.querySelector('#add-book');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -60,11 +64,14 @@ form.addEventListener('submit', (event) => {
       autor: bookAutor.value,
       title: bookTitle.value,
     };
-    newCollection(bookValues);
-    showBooks();
+    awsomeBooks.newCollection(bookValues);
+    awsomeBooks.showBooks();
     bookTitle.value = '';
     bookAutor.value = '';
   }
 });
-
-showBooks();
+const remove = (index) => {
+  awsomeBooks.deleteCollection(index);
+};
+remove(-1);
+awsomeBooks.showBooks();
