ea948a6a385222d90d281e0d103c2a367a0b6a19
darikmohammed <r.m.darik@gmail.com>
2022-05-25 19:08:05 +0300

    Add styling for the page

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ class AwsomeBooks {
   }
 
   #SetBooksList = (books) => {
-    localStorage.setItem('Books', JSON.stringify(books));
+    localStorage.setItem("Books", JSON.stringify(books));
   };
 
   #getBooksList = () => {
-    const books = JSON.parse(localStorage.getItem('Books'));
+    const books = JSON.parse(localStorage.getItem("Books"));
     if (books) {
       return books;
     }
@@ -17,13 +17,13 @@ class AwsomeBooks {
 
   showBooks = () => {
     const books = this.#getBooksList();
-    const htmlbooks = document.querySelector('.books');
-    htmlbooks.innerHTML = '';
+    const htmlbooks = document.querySelector(".books");
+    htmlbooks.innerHTML = "";
     for (let i = 0; i < books.length; i += 1) {
       htmlbooks.innerHTML += `
-          <div class="book book${i % 2}">
+          <div class="book">
           <h3>"${books[i].title}"</h3>
-          <p>by ${books[i].autor}</p>
+          <p><span class="by-span"> By </span> ${books[i].autor}</p>
           <button type="button" onClick= "remove(${i})" class='removebtn'>Remove</button>
           `;
     }
@@ -53,21 +53,21 @@ class AwsomeBooks {
 
 const awsomeBooks = new AwsomeBooks();
 
-const form = document.querySelector('#add-book');
-form.addEventListener('submit', (event) => {
+const form = document.querySelector("#add-book");
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const bookTitle = document.querySelector('#title');
-  const bookAutor = document.querySelector('#autor');
+  const bookTitle = document.querySelector("#title");
+  const bookAutor = document.querySelector("#autor");
 
-  if (bookTitle.value.trim() !== '' && bookAutor.value.trim() !== '') {
+  if (bookTitle.value.trim() !== "" && bookAutor.value.trim() !== "") {
     const bookValues = {
       autor: bookAutor.value,
       title: bookTitle.value,
     };
     awsomeBooks.newCollection(bookValues);
     awsomeBooks.showBooks();
-    bookTitle.value = '';
-    bookAutor.value = '';
+    bookTitle.value = "";
+    bookAutor.value = "";
   }
 });
 const remove = (index) => {
